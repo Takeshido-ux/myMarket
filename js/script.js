34174f5db63c90f1726ee7bfcfe457700904ae48
Takeshido-ux <isma555555655@gmail.com>
2021-11-02 17:52:50 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -2,10 +2,53 @@ $('.header-btn').click(function(){
     $('.header-btn').toggleClass('header-btn__active');
 })
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $(".order__slider").owlCarousel({
         items: 1,
         dots: true,
         nav: true,
         loop: true
     });
 });
+$(document).ready(function(){
+    $(".delivery__slider").owlCarousel({
+        dots: true,
+        loop: true,
+        URLhashListener:true,
+        responsiveClass:true,
+        responsive: {
+            0 : {
+                items: 0,
+                nav: false
+            },
+            567 : {
+                items: 1,
+                nav: true
+            }
+        }
+    });
+});
+$(document).ready(function(){
+    $(".delivery__slider-mob").owlCarousel({
+        dots: true,
+        nav: false,
+        loop: true,
+        URLhashListener:true,
+        responsiveClass:true,
+        responsive: {
+            0 : {
+                items: 1,
+            },
+            567 : {
+                items: 0,
+            }
+        }
+    });
+});
+$('.hash-nav__link').click(function(){
+    $('.hash-nav__link').removeClass('hash-link__active');
+    $(this).addClass('hash-link__active'); 
+})
+$('.hash-nav__text').click(function(){
+    $('.hash-nav__text').removeClass('hash-text__active');
+    $(this).addClass('hash-text__active');
+})
